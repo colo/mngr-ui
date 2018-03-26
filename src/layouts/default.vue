e99f3167750cc2d4952b57c72f2956263e735c44
Luciano Bueno <glamero@gmail.com>
2018-03-25 22:24:52 -0300

    bump

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -33,6 +33,52 @@
       </q-toolbar>
 
     </q-layout-header>
+    
+    <q-layout-drawer
+       v-model="rightDrawerOpen"
+       side="right"
+    >
+          <!-- :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null" -->
+      <q-list
+        no-border
+        link
+        inset-delimiter
+      >
+        <!-- <q-list-header>Essential Links</q-list-header> -->
+        <q-item>
+          <at-select v-model="selectedHost"
+            filterable
+            size="large"
+            style="width: 240px"
+            v-for="(host) in selectHosts"
+            :key="host.value"
+            >
+            <at-option value="host.value">{{host.label}}</at-option>
+          </at-select>
+
+        </q-item>
+        <q-item @click.native="openURL('http://quasar-framework.org')">
+          <q-item-side icon="school" />
+          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+        </q-item>
+        <q-item @click.native="openURL('https://github.com/quasarframework/')">
+          <q-item-side icon="code" />
+          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
+        </q-item>
+        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
+          <q-item-side icon="chat" />
+          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
+        </q-item>
+        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
+          <q-item-side icon="record_voice_over" />
+          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+        </q-item>
+        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
+          <q-item-side icon="rss feed" />
+          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        </q-item>
+      </q-list>
+    </q-layout-drawer>
 
     <q-layout-drawer
        v-model="leftDrawerOpen"
@@ -79,9 +125,11 @@
       </q-list>
     </q-layout-drawer>
 
+
     <q-page-container>
       <router-view />
     </q-page-container>
+
 
     <!-- <q-page-sticky position="top-right" :offset="[18, 18]">
       <at-select v-model="selectedHost"
@@ -105,6 +153,7 @@ export default {
   data () {
     return {
       leftDrawerOpen: this.$q.platform.is.desktop,
+      rightDrawerOpen: this.$q.platform.is.desktop,
       selectedHost: 0,
       selectHosts: [
         {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,31 @@
 <template>
   <q-page class="flex flex-center">
-    <img v-if="!$q.platform.is.desktop" alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-    <at-rate v-else></at-rate>
+    <!--
+    <at-rate v-else></at-rate> -->
+    <template v-if="!$q.platform.is.desktop">
+      <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    </template>
+    <template v-else>
+    <at-collapse simple :value="[0, 1]">
+      <at-collapse-item
+        v-for="(item, index) in list"
+        :key="index"
+
+      >
+
+          <div slot="title">{{item.title}} <i class="icon icon-info"/></div>
+          <at-card style="width: 300px;" :bordered="false">
+            <h4 slot="title">Card Title</h4>
+            <div slot="extra"><a>Extra</a></div>
+            <div>
+              Card Content
+            </div>
+          </at-card>
+          <!-- {{ item.content }} -->
+
+      </at-collapse-item>
+    </at-collapse>
+  </template>
   </q-page>
 </template>
 
@@ -11,6 +35,14 @@
 <script>
 
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      list: [
+        {title: 'first chart'},
+        {title: 'second chart'},
+      ]
+    }
+  }
 }
 </script>
